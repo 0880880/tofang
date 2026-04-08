0239e5e90f220664ea5a4c7fb7bac279c17b2ec9
0880 <98263509+0880880@users.noreply.github.com>
2026-04-08 14:49:35 +0430

    make declarations public

--- include/symbols.h
+++ include/symbols.h
@@ -3,10 +3,9 @@
 #include "ast.h"
 
 class Symbols {
-private:
+public:
   deque<unordered_map<string, Decl *>> declarations = {{}};
 
-public:
   void open(ASTNode *node);
   void close(ASTNode *node);
 };
