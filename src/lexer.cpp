549495fe748a9a228a34eb174f092ed7f498c49e
0880 <98263509+0880880@users.noreply.github.com>
2026-05-31 14:49:59 +0330

    fix ub

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -7,52 +7,53 @@
 
 using namespace std;
 
-size_t getLine(const string &text, size_t pos) {
-  return 1 +
-         static_cast<uint64_t>(count(text.begin(), text.begin() + pos, '\n'));
+size_t getLine(const string& text, size_t pos)
+{
+    return 1 + static_cast<uint64_t>(count(text.begin(), text.begin() + pos, '\n'));
 }
 
-void Lexer::token(string re, string type) {
-  tokenTypes.push_back({.pattern = std::move(re), .type = std::move(type)});
+void Lexer::token(string re, string type)
+{
+    tokenTypes.push_back({ .pattern = std::move(re), .type = std::move(type) });
 }
 
-vector<Lexer::Token> Lexer::tokenize(const string &source) {
-  string pat;
-  pat.reserve(30 * tokenTypes.size());
+vector<Lexer::Token> Lexer::tokenize(const string& source)
+{
+    string pat;
+    pat.reserve(30 * tokenTypes.size());
 
-  for (size_t i = 0; i < tokenTypes.size(); ++i) {
-    if (i) {
-      pat.append("|");
+    for (size_t i = 0; i < tokenTypes.size(); ++i) {
+        if (i) {
+            pat.append("|");
+        }
+        pat.append("(");
+        pat.append(tokenTypes[i].pattern);
+        pat.append(")");
     }
-    pat.append("(");
-    pat.append(tokenTypes[i].pattern);
-    pat.append(")");
-  }
 
-  auto no_ws = pat | views::filter([](unsigned char c) { return !isspace(c); });
+    auto no_ws = pat | views::filter([](unsigned char c) { return !isspace(c); });
+    std::string clean_pat(no_ws.begin(), no_ws.end());
+    pat = std::move(clean_pat);
+    regex pattern(pat);
 
-  pat.assign(no_ws.begin(), no_ws.end());
+    auto begin = sregex_iterator(source.begin(), source.end(), pattern);
+    auto end = sregex_iterator();
 
-  regex pattern(pat);
+    vector<Lexer::Token> tokens;
 
-  auto begin = sregex_iterator(source.begin(), source.end(), pattern);
-  auto end = sregex_iterator();
-
-  vector<Lexer::Token> tokens;
-
-  for (std::sregex_iterator i = begin; i != end; ++i) {
-    const smatch &match = *i;
-    auto sub = match.str(0);
-    for (size_t j = 1; j < match.size(); ++j) {
-      auto g = match.str(j);
-      if (g != "") {
-        auto m_start = static_cast<size_t>(match.position(0));
-        auto m_end = static_cast<size_t>(match.position(0)) + sub.length();
-        tokens.emplace_back(tokenTypes[j - 1].type, sub, m_start, m_end,
-                            getLine(source, m_start), getLine(source, m_end));
-        break;
-      }
+    for (std::sregex_iterator i = begin; i != end; ++i) {
+        const smatch& match = *i;
+        auto sub = match.str(0);
+        for (size_t j = 1; j < match.size(); ++j) {
+            auto g = match.str(j);
+            if (g != "") {
+                auto m_start = static_cast<size_t>(match.position(0));
+                auto m_end = static_cast<size_t>(match.position(0)) + sub.length();
+                tokens.emplace_back(tokenTypes[j - 1].type, sub, m_start, m_end,
+                    getLine(source, m_start), getLine(source, m_end));
+                break;
+            }
+        }
     }
-  }
-  return tokens;
+    return tokens;
 }
