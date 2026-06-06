6fe99494908f339dc9909e02990bb5908d0463d7
0880 <98263509+0880880@users.noreply.github.com>
2026-06-06 17:08:59 +0330

    Access Path refinement

--- include/typechecker.h
+++ include/typechecker.h
@@ -1,10 +1,32 @@
 #pragma once
 #include "ast.h"
 
-struct RefinementEnv {
-  std::unordered_map<Decl *, TypeThing *> map;
+struct AccessPath
+{
+    Decl *root;
+  std::vector<std::string> fields;
 
-  TypeThing *lookup(Decl *d);
+  bool operator==(const AccessPath &other) const
+  {
+    return root == other.root && fields == other.fields;
+  }
+};
+
+struct AccessPathHash {
+  std::size_t operator()(const AccessPath& path) const {
+    std::size_t res = std::hash<Decl*>()(path.root);
+    for (const auto& field : path.fields) {
+      // Standard hash combining
+      res ^= std::hash<std::string>()(field) + 0x9e3779b9 + (res << 6) + (res >> 2);
+    }
+    return res;
+  }
+};
+
+struct RefinementEnv {
+  std::unordered_map<AccessPath, TypeThing *, AccessPathHash> map;
+
+  TypeThing *lookup(const AccessPath &d);
 };
 
 class TypeChecker {
